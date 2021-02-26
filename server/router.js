94742a94acd411d581a32426b74c74caf788e513
codefactor-io <support@codefactor.io>
2021-02-26 05:50:38 +0000

    [CodeFactor] Apply fixes

--- server/router.js
+++ server/router.js
@@ -49,7 +49,7 @@ router.get('*', (req, res, next) => {
 			else {
 				page += '/index';
 				return getData(page);
-			};
+			}
 		})
 		.then((pageData) => ({
 			headTitle: headData(pageData, 'title'),
